ce1ee30d6da4a9fa3fa3f48acacb57a7731ccfd8
Haile-Baranto <hailebaranto@gmail.com>
2023-03-13 17:24:06 +0300

    add soln to 0-print_list_integer.py

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def print_list_integer(my_list=[]):
+    """Write a function that prints all integers of a list.
+        Prototype: def print_list_integer(my_list=[]):
+        Format: one integer per line. See example
+        You are not allowed to import any module
+        You can assume that the list only contains integers
+        You are not allowed to cast integers into strings
+        You have to use str.format() to print integers"""
+    for i in my_list:
+        print("{}".format(i))
