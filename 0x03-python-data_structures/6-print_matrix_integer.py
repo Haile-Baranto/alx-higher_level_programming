fda6adef797cb72215a0780a29527f62eead04da
Haile-Baranto <hailebaranto@gmail.com>
2023-03-13 18:15:22 +0300

    Edit soln of 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,8 +2,7 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        new_list = i
-        for j in range(len(new_list) - 1):
-            print("{:d}, ".format(new_list[j]), end='')
-            print(new_list[len(new_list) - 1])
+    for row in matrix:
+        for column in row:
+            print("{:d}".format(column), end="" if column == row[-1] else " ")
+        print()
