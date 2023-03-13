88f37c56b8d1594017168c6565dbcc559e36cd9c
Haile-Baranto <hailebaranto@gmail.com>
2023-03-13 21:14:57 +0300

    Edit soln to 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -13,7 +13,9 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_a (tuple, optional): _description_. Defaults to ().
         tuple_b (tuple, optional): _description_. Defaults to ().
     """
+    two_element_tuple = ()
     zero_tuple = (0, 0)
-    tuple_one = tuple_a + zero_tuple
-    tuple_two = tuple_b + zero_tuple
-    two_element_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    tuple_1 = tuple_a + zero_tuple
+    tuple_2 = tuple_b + zero_tuple
+    two_element_tuple = (tuple_1[0] + tuple_2[0], tuple_1[1] + tuple_2[1])
+    return two_element_tuple
