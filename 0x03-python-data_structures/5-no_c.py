4b18a7461abc57f4fe73f91ce829472b4a41fef7
Haile-Baranto <hailebaranto@gmail.com>
2023-03-13 18:03:28 +0300

    Edit soln of 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,14 +2,5 @@
 
 
 def no_c(my_string):
-    char_list = []
-    for charachter in my_string:
-        char_list.append(charachter)
-
-    for item in char_list:
-        if item == 'c' or item == 'C':
-            char_list.remove(item)
-    my_new_string = ''
-    for item in char_list:
-        my_new_string += item
-    return my_new_string
+    no_Cc_string = my_string.translate({ord(i): None for i in 'cC'})
+    return no_Cc_string
