245cb4dbba2e1ba66a0ca5cfd4cd248d834e2f47
Haile-Baranto <hailebaranto@gmail.com>
2023-04-04 05:42:49 +0300

    Edit pycodestyle of 9-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -117,6 +117,7 @@ class Rectangle(object):
         if rect_1.area() >= rect_2.area():
             return rect_1
         return rect_2
+
     @classmethod
     def square(cls, size=0):
         """Returns a new instance of Rectangle with
