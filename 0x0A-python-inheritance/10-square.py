8a81cc84e5935ae50583772de3517b5df0d3427f
Haile-Baranto <hailebaranto@gmail.com>
2023-04-10 22:30:42 +0300

    Add solution to 10-square.py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" The module contains class Square that inherits from
+        Rectangle (9-rectangle.py
+    importing any module is not allowed
+    """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class Square inherits from super class Rectangle
+
+    Args:
+        Rectangle (cls): class of type Rectangle
+    """
+    def __init__(self, size):
+        
+        self.integer_validator("size", size)
+        self.__size = size
+        
+    def area(self):
+        """Returns the area of rectangle
+
+        Returns:
+            int: rectangle area
+        """
+        return self.__size * self.__size
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
