15a984dd05966149c0a9d8528536725847c3713e
Haile-Baranto <hailebaranto@gmail.com>
2023-04-12 01:19:42 +0300

    Edit solution to 4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-
 import json
-
-""" The module contains from_json_string that converts
-    json file to python data sructure
+""" The function converts from json to string
     Prototype: def from_json_string(my_str):
-    You don’t need to manage exceptions if the JSON string
-        doesn’t represent an object.
+    You don’t need to manage exceptions if the JSON
+        string doesn’t represent an object.
     """
 
 
 def from_json_string(my_str):
+    """
+    Returns the Python data structure represented by a JSON string.
+
+    Args:
+        my_str (str): The JSON string to parse.
+
+    Returns:
+        object: The Python data structure represented by the JSON string.
+    """
     return json.loads(my_str)
