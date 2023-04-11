ee0dcaad493706895c06bdcd6db9a93add8c6067
Haile-Baranto <hailebaranto@gmail.com>
2023-04-12 01:07:33 +0300

    Add solution to 9-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+    The module contains a class Student
+    """
+class Student:
+    """A class representing a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new student object.
+
+        Args:
+            first_name (str): The student's first name.
+            last_name (str): The student's last name.
+            age (int): The student's age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Return a dictionary representation of the student."""
+        student_dict = {
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "age": self.age
+        }
+        return student_dict
