c0c9aaf72a7d911d9e7188fa1a1b5d93bb3bca84
Haile-Baranto <hailebaranto@gmail.com>
2023-04-11 16:01:03 +0300

    Add solution to 3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import json
+""" The module contains to_json_string function that converts
+        python data to json
+    Prototype: def to_json_string(my_obj):
+    You don’t need to manage exceptions if the object can’t be serialized.
+
+    """
+
+
+def to_json_string(my_obj):
+    """
+
+    Args:
+        my_obj (obj): python object to be converted to json
+
+    Returns:
+        json: json representation of my_obj
+    """
+    return json.dumps(my_obj)
