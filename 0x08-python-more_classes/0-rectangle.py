652505b512d51c93822b267d401671201879c4a3
HabtamuZegeye <habtamuzegeye48@gmail.com>
2023-06-05 17:22:45 +0300

    Removed folder from repository

--- 0x08-python-more_classes/0-rectangle.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-# creates an empty class
-""" Define Rectangle class
-    Using module is not allowed"""
-
-
-class Rectangle(object):
-    """ defines new class Rectangle"""
-    pass
