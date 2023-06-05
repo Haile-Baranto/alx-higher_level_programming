b7ba0e634e3281a9fafe274193975811a20499f4
HabtamuZegeye <habtamuzegeye48@gmail.com>
2023-06-05 18:32:45 +0300

    Add solution to 0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# creates an empty class
+""" Define Rectangle class
+    Using module is not allowed"""
+
+
+class Rectangle(object):
+    """ defines new class Rectangle"""
+    pass
