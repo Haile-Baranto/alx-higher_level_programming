00528ada50af333a2d91dc6263bffaa33dedce16
Haile-Baranto <habtamuzegeye48@gmail.com>
2023-06-12 15:25:28 +0300

    Add solution to 5-base_geometry.py

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" The module contains empty class BaseGeometry.
+    You are not allowed to import any module
+    """
+
+
+class BaseGeometry:
+    """Defines an empty BaseGeomery class
+    """
+    pass
