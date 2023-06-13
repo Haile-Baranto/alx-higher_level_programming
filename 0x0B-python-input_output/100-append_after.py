db77ed1db41471fa3f46c58b2f988a09a85ec713
Haile-Baranto <hailebaranto@gmail.com>
2023-06-13 19:30:29 +0300

    Edit soln to 100-append_after.py

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,35 +1,26 @@
 #!/usr/bin/python3
 """
-File Append After
-
-This module provides a function to insert a line of text
-after each line containing a specific string in a file.
-
-Example:
-    # Insert "New line" after each line containing
-    # "old line" in the file "example.txt"
-    append_after("example.txt", "old line", "New line")
-
+Function that inserts a line of text to a file, after each
+line containing a specific string.
 """
 
 
 def append_after(filename="", search_string="", new_string=""):
     """
-    Insert a line of text after each line containing
-    a specific string in a file.
+    Insert a line of text after each line containing a specific string.
 
     Args:
         filename (str): The name of the file.
         search_string (str): The string to search for in each line.
-        new_string (str): The line of text to insert after each matching line.
+        new_string (str): The line of text to insert.
 
     """
-    lines = []
-    with open(filename, 'r') as file:
-        for line in file:
-            lines.append(line)
+    with open(filename, 'r+') as file:
+        lines = file.readlines()
+        i = 0
+        for line in lines:
             if search_string in line:
-                lines.append(new_string + '\n')
-
-    with open(filename, 'w') as file:
+                lines.insert(i + 1, new_string)
+            i += 1
+        file.seek(0)
         file.writelines(lines)
