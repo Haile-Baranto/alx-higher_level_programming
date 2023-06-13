5b9f8e7f0a36c03485d6b80e960b5e4434db31b8
Haile-Baranto <hailebaranto@gmail.com>
2023-06-13 18:33:18 +0300

    Add soln to 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,25 +7,15 @@ This script adds all command-line arguments to a Python
 list and saves them to a JSON file.
 """
 
-import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+if __name__ == "__main__":
+    import sys
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file =\
+        __import__('6-load_from_json_file').load_from_json_file
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
 
-# Load existing items from the JSON file, if it exists
-try:
-    items = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    items = []
-
-# Add command-line arguments to the list
-items.extend(sys.argv[1:])
-
-# Save the updated list to the JSON file
-save_to_json_file(items, "add_item.json")
-
-# Display the contents of the JSON file
-try:
-    items = load_from_json_file("add_item.json")
-    print(items)
-except FileNotFoundError:
-    print("[]")
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
