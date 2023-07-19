0be47c7375b28c1f04e27e0b5f4ef9f96260f7a4
Haile-Baranto <hailebaranto@gmail.com>
2023-07-19 22:19:32 +0300

    Add 10-genre_id_by_show.sql

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,9 @@
+-- Script: List Shows with Linked Genres
+-- Description: This script lists all the shows contained in the hbtn_0d_tvshows
+-- database that have at least one genre linked.
+
+SELECT show.`title`, genere.`genre_id`
+  FROM `tv_shows` AS show
+        INNER JOIN `tv_show_genres` AS genere
+	ON show.`id` = genere.`show_id`
+ ORDER BY show.`title`, genere.`genre_id`;
