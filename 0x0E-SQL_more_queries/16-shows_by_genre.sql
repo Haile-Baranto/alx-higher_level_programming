d9a166aca31a4197910027aa73576af591c265b6
Haile-Baranto <hailebaranto@gmail.com>
2023-07-19 22:53:06 +0300

    Add comment

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,3 +1,5 @@
+-- Script: Shows and Linked Genres
+-- Description: This script lists all shows and their linked genres from the hbtn_0d_tvshows database.
 SELECT tv.`title`, genere.`name`
   FROM `tv_shows` AS tv
        LEFT JOIN `tv_show_genres` AS show
