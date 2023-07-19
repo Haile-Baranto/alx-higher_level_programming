25d846d2c356ea41877a2cf12ab40171fcd0af8b
Haile-Baranto <hailebaranto@gmail.com>
2023-07-19 19:05:04 +0300

    fix backtick bug

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -2,7 +2,7 @@
 -- ordered by temperature in descending order.
 
 -- Compute the average temperature by city
-SELECT city, AVG(temperature) AS avg_temp
-FROM weather_data
-GROUP BY city
-ORDER BY avg_temp DESC;
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+GROUP BY `city`
+ORDER BY `avg_temp` DESC;
