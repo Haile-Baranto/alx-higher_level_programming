08903e244e5cfb91858a12f2e96c12c58138bed6
Haile-Baranto <hailebaranto@gmail.com>
2023-07-19 22:22:04 +0300

    fix syntax error

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -6,4 +6,4 @@ SELECT show.`title`, genere.`genre_id`
   FROM `tv_shows` AS show
         INNER JOIN `tv_show_genres` AS genere
 	ON show.`id` = genere.`show_id`
- ORDER BY show.`title`, genere.`genre_id`;
+ ORDER BY show.`title`, genere.`genere_id`;
