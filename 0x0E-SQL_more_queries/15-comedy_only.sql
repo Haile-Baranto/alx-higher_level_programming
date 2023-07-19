241a1776ce179d094044af437af09fd9218c9bab
Haile-Baranto <hailebaranto@gmail.com>
2023-07-19 22:51:50 +0300

    Add comment

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,3 +1,5 @@
+-- Script: Comedy Shows
+-- Description: This script lists all Comedy shows from the hbtn_0d_tvshows database.
 SELECT tv.`title`
   FROM `tv_shows` AS tv
        INNER JOIN `tv_show_genres` AS show
