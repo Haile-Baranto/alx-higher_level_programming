d33b3236c13d9bac7db20dc6f2a64db2a4081377
Haile-Baranto <hailebaranto@gmail.com>
2023-07-19 18:41:27 +0300

    Add 10-top_score.sql

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- This script lists all records of the table second_table from the hbtn_0c_0 
+-- database, ordered by score (top first).
+
+-- List all records ordered by score
+SELECT score, name FROM second_table ORDER BY score DESC;
