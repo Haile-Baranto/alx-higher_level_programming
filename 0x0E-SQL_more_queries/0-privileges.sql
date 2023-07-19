312bf57b1af5f4132f5400835355b0cb6d64919b
Haile-Baranto <hailebaranto@gmail.com>
2023-07-19 21:30:06 +0300

    Add 0-privileges.sql

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,8 @@
+-- Script: List User Privileges
+-- Description: This script lists all privileges of the specified MySQL users on the server.
+
+-- List privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- List privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
