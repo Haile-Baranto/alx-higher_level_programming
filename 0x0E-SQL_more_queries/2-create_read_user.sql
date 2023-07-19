f441ceb5e1e3eb74372cc3ad5a966a96cb998324
Haile-Baranto <hailebaranto@gmail.com>
2023-07-19 21:43:55 +0300

    Add 2-create_read_user.sql

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,14 @@
+-- Script: Create Database and User with SELECT Privilege
+-- Description: This script creates the database and user with SELECT privilege.
+
+-- Create database hbtn_0d_2 if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create user_0d_2 if it doesn't exist
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant SELECT privilege to user_0d_2 on hbtn_0d_2 database
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+-- Flush privileges to apply changes
+FLUSH PRIVILEGES;
