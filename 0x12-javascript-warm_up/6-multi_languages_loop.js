b7b935cffd86097dd0fdd10f8de066cef981f26f
Haile-Baranto <hailebaranto@gmail.com>
2023-08-14 20:55:23 +0300

    Add 6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/** Write a script that prints 3 lines:
+ * The first line: “C is fun”
+ * The second line: “Python is cool”
+ * The third line: “JavaScript is amazing”
+ * You must use console.log and (while or for loops)
+ * Not allowed to use if/else and var
+ */
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < languages.length; i++) {
+  console.log(languages[i]);
+}
