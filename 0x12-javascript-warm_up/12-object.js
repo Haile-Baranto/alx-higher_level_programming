f7d0d25687d0fbb8264357065b1990e0575a9f26
Haile-Baranto <hailebaranto@gmail.com>
2023-08-14 20:58:32 +0300

    Add 12-object.js

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/**
+ * Update this script to replace the value 12 with 89:
+ * You are not allowed to use var
+ * the following code is given
+ */
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89; // line of code that changes value of value to 89
+console.log(myObject);
