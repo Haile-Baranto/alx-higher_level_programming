fb7a9ab50a2803962e28047b932bef02dd26b837
Haile-Baranto <hailebaranto@gmail.com>
2023-08-14 20:53:27 +0300

    Add 1-multi_languages.js

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/** Write a script that prints 3 lines:
+ * The first line: “C is fun”
+ * The second line: “Python is cool”
+ * The third line: “JavaScript is amazing”
+ * You must use console.log but not var
+ */
+const myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
+console.log(myVar);
