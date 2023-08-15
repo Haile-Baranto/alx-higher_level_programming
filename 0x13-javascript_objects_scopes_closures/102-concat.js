9b6ba2ba9fd29e103847aac42d5b8d7520384aa2
Haile-Baranto <hailebaranto@gmail.com>
2023-08-15 17:46:54 +0300

    Optimize 102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,41 +1,29 @@
 #!/usr/bin/node
-
-/**
- * Write a script that concats 2 files.
- *      The first argument is the file path of the first source file
-        The second argument is the file path of the second source file
-        The third argument is the file path of the destination
- */
 const fs = require('fs');
 
-const args = process.argv.slice(2);
-if (args.length !== 3) {
-  console.error('Usage: ./102-concat.js <file1> <file2> <destination>');
-  process.exit(1);
+// Get the file paths from command-line arguments
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const fileC = process.argv[4];
+
+// Check if files exist, are valid, and if a destination is provided
+if (
+  fs.existsSync(fileA) &&
+  fs.statSync(fileA).isFile() &&
+  fs.existsSync(fileB) &&
+  fs.statSync(fileB).isFile() &&
+  fileC !== undefined
+) {
+  // Read the contents of the source files
+  const fileAContent = fs.readFileSync(fileA);
+  const fileBContent = fs.readFileSync(fileB);
+
+  // Write the concatenated content to the destination file
+  fs.writeFileSync(fileC, fileAContent + fileBContent);
+
+  // Print a success message
+  console.log(`Concatenated data has been written to ${fileC}`);
+} else {
+  // Display a usage error message
+  console.error('Usage: ./concat.js <file1> <file2> <destination>');
 }
-
-const [file1, file2, destination] = args;
-
-fs.readFile(file1, 'utf8', (err, data1) => {
-  if (err) {
-    console.error(`Error reading ${file1}: ${err.message}`);
-    process.exit(1);
-  }
-
-  fs.readFile(file2, 'utf8', (err, data2) => {
-    if (err) {
-      console.error(`Error reading ${file2}: ${err.message}`);
-      process.exit(1);
-    }
-
-    const concatenatedData = data1 + data2;
-
-    fs.writeFile(destination, concatenatedData, 'utf8', (err) => {
-      if (err) {
-        console.error(`Error writing to ${destination}: ${err.message}`);
-        process.exit(1);
-      }
-      console.log(`Concatenated data has been written to ${destination}`);
-    });
-  });
-});
