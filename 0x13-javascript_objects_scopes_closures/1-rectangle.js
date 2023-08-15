af000d57638034a3cc591b33db23419c011bbe4c
Haile-Baranto <hailebaranto@gmail.com>
2023-08-15 15:14:41 +0300

    Add 1-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/** The module defines class Rectangle with constructor which
+ *  takes the arguements "w and h" to intialize height and width of rectangle
+ */
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
