06182f1e214788308120d11350752d464562c381
Haile-Baranto <hailebaranto@gmail.com>
2023-08-15 16:04:56 +0300

    fix rotate() method

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -28,13 +28,13 @@ class Rectangle {
   }
 
   rotate () {
-    this.width = this.height;
-    this.height = this.width;
+    [this.width, this.height] = [this.height, this.width];
+    
   }
 
   double () {
-    this.width = 2 * this.width;
-    this.height = 2 * this.height;
+    this.width *= 2;
+    this.height *= 2;
   }
 }
 module.exports = Rectangle;
