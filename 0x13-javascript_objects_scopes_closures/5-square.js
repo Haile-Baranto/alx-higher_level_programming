5fcd8ffcb57608635e400b20d494c0cc8d76e60f
Haile-Baranto <hailebaranto@gmail.com>
2023-08-15 16:33:17 +0300

    change class name from Squre to Square

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
+const Rectangle = require('./4-rectangle'); // import Rectangle class
 /**
  * The class Squre inhertis from Rectangle class of 4-rectangle.js
  * The constructor takes one arguement called size and rectangle constructor is
  * called using  super()
  */
 
-const Rectangle = require('./4-rectangle'); // import Rectangle class
-class Squre extends Rectangle {
+class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
 }
-module.exports = Squre;
+module.exports = Square;
