b89833a5e32af742b409e927ef1facb6897501ba
Haile-Baranto <hailebaranto@gmail.com>
2023-08-15 16:28:55 +0300

    fix style of 5-square.js

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
-
-const Rectangle = require('./4-rectangle'); // import Rectangle class
 /**
  * The class Squre inhertis from Rectangle class of 4-rectangle.js
  * The constructor takes one arguement called size and rectangle constructor is
  * called using  super()
  */
 
+const Rectangle = require('./4-rectangle'); // import Rectangle class
 class Squre extends Rectangle {
   constructor (size) {
     super(size, size);
