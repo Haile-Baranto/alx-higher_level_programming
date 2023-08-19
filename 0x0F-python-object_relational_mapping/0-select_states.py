e68af6e7395aac30f497bbbd70c82a29419f9d51
Haile-Baranto <hailebaranto@gmail.com>
2023-08-19 15:28:12 +0300

    Add 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,39 +1,38 @@
 #!/usr/bin/python3
+
+""" The script displayes all states by ID using
+mysql username, mysql password and database name as arguement
+from the command line"""
 import MySQLdb
-import sys  # to  accept arguement from the command line
-""" The script displayes all states by ID """
+import sys
 
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <mysql_username> <mysql_password>\
+              <database_name>")
+        sys.exit(1)
 
-def list_states(username, password, database_name):
-    """The function accepts username, password and name of the
-    database make conection to the database and list states by ID"""
-    # connect to the database
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    # Connect to the MySQL server
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=username,
-        password=password,
+        passwd=password,
         db=database_name)
     cursor = db.cursor()
 
-    # Retrieve and display list of states from table state
+    # Retrieve and display sorted list of states
     query = "SELECT * FROM states ORDER BY states.id ASC"
     cursor.execute(query)
     states = cursor.fetchall()
 
     for state in states:
         print(state)
-    # close the database connection
+
+    # Close the database connection
     cursor.close()
     db.close()
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: python script.py <mysql_username> <mysql_password>\
-              <database_name>")
-    else:
-        username = sys.argv[1]
-        password = sys.argv[2]
-        database_name = sys.argv[3]
-        list_states(username, password, database_name)
