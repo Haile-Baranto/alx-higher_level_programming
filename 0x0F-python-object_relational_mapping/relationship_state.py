851c61c1e615c7364b9e6397e07ffb2cf2d1524a
Haile-Baranto <hailebaranto@gmail.com>
2023-08-20 22:11:43 +0300

    Add relationship_city.py relationship_state.py

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+State class definition with relationship to City
+"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from relationship_city import Base, City
+
+
+class State(Base):
+    """State class that inherits from Base"""
+
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all,\
+                          delete-orphan")
