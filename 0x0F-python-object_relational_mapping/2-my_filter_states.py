73a049d00fd2bb2c8dddc279568bcdc595f9e85b
Haile-Baranto <hailebaranto@gmail.com>
2023-08-20 12:48:23 +0300

    Add  2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+Script that takes in an argument and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument.
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: {} name passwd database state_name".format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connect to MySQL
+    db = MySQLdb.connect(host="localhost", user=username,
+                         passwd=password, db=database, port=3306)
+
+    # Create a cursor to execute SQL queries
+    cursor = db.cursor()
+
+    # Execute the SQL query with user input
+    query = "SELECT * FROM states WHERE name=%s ORDER BY id"
+    cursor.execute(query, (state_name,))
+
+    # Fetch all the rows
+    rows = cursor.fetchall()
+
+    # Display the results
+    for row in rows:
+        print(row)
+
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
