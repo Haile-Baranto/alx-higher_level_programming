235fd7ca6d8f2957cd9f0c543e932c63825966f7
Haile-Baranto <hailebaranto@gmail.com>
2023-08-20 22:22:05 +0300

    Update 100-relationship_states_cities.py

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,36 +1,35 @@
 #!/usr/bin/python3
 """
-Script that creates the State "California" with the City "San Francisco"
-in the database hbtn_0e_100_usa
+Creates a State with a City using SQLAlchemy relationships
 """
 
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sys import argv
 from relationship_state import Base, State
 from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} username password database".format(sys.argv[0]))
-        exit(1)
+    # Create a connection to the database
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
-
-    # Create an engine to connect to the database
-    engine = create_engine(
-        f'mysql+mysqldb://{username}:{password}@localhost/{database}'
-    )
-
-    # Create all tables
+    # Create the tables defined in the Base class
     Base.metadata.create_all(engine)
 
     # Create a session to interact with the database
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    session = Session(engine)
 
-    # Create a new State with the City and add them to the session
-    new_state = State(name="California", cities=[City(name="San Francisco")])
+    # Create a new State object named "California"
+    new_state = State(name='California')
+
+    # Create a new City object named "San Francisco"
+    new_city = City(name='San Francisco')
+
+    # Associate the new City with the new State using relationships
+    new_state.cities.append(new_city)
+
+    # Add the State object to the session and commit the changes
     session.add(new_state)
     session.commit()
 
