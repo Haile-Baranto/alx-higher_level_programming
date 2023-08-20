723680c2783062e19206b1b5ba5d673bd230d015
Haile-Baranto <hailebaranto@gmail.com>
2023-08-20 22:17:02 +0300

    Add relationship_state.py

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -14,5 +14,5 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all,\
-                          delete-orphan")
+    cities = relationship('City', cascade='save-update, merge, delete',
+                          backref='state')
