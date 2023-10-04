4389f950832b9dd18fb98741f9ce0fcc332e4cdd
Haile-Baranto <hailebaranto@gmail.com>
2023-10-04 16:37:38 +0300

    Add 100-script.js

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+// Change the text color of the <header> element to red when the HTML document is fully loaded
+
+document.addEventListener('DOMContentLoaded', function () {
+  const headerElement = document.querySelector('header');
+  headerElement.style.color = '#FF0000';
+});
